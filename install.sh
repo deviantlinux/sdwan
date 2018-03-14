bc319153b763560a138f8d1db020b71b47ff2f3f
deviantlinux <37279798+deviantlinux@users.noreply.github.com>
2018-03-14 14:54:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 #
 echo "Please enter mode. Expected Values are server or client"
 read -r MODE_INPUT
-export MODE=$SITE_INPUT
+export MODE=$MODE_INPUT
 if [ -z $MODE ] ; then echo "OpenVPN mode not declared. Exiting" ; exit ; fi
 if [ $MODE = server ] ; then echo $MODE >> $MODE.conf; elif [ $MODE = client ] ; then echo $MODE >> $MODE.conf; else echo error $SITE is unexpected; exit; fi 

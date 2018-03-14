182cfb33940ea97513733f477c80540c5b15134b
deviantlinux <37279798+deviantlinux@users.noreply.github.com>
2018-03-14 15:05:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,4 +7,3 @@ export MODE=$MODE_INPUT
 if [ -z $MODE ] ; then echo "OpenVPN mode not declared. Exiting" ; exit ; fi
 if [ $MODE = server ] ; then echo $MODE > $MODE.conf; elif [ $MODE = client ] ; then echo $MODE > $MODE.conf; else echo error $MODE is unexpected; exit; fi 
 if [ -z $CN ] ; then echo "Client name not decalred. Please check env.list"; fi
-echo $CN

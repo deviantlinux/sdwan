35cf17df4be8aa006cd868026508bec42f4b1b75
deviantlinux <37279798+deviantlinux@users.noreply.github.com>
2018-03-14 18:04:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,13 @@ echo "Please enter mode. Expected Values are server or client"
 read -r MODE_INPUT
 export MODE=$MODE_INPUT
 
-docker run -d -e $MODE --env-file env.list --name sdwan deviantlinux/sdwan:latest
+if [[ $MODE = client ]] ; then
+#docker run -d -e $MODE --env-file env.list --name sdwan deviantlinux/sdwan:latest
+# or
+# docker run --network=host --privileged -d --name ovpn deviantlinux/ovpnclient
+#not sure
+#Need the priv stuff. brain all over the palce at the moment
+
+else 
+docker run -d -e $MODE --env-file --name -- someother stuff specific to server conf
+#!Fuck sake

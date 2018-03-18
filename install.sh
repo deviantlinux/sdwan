33f3f5bf7a6ad4356d774ca27c62804dd1302eea
deviantlinux <37279798+deviantlinux@users.noreply.github.com>
2018-03-18 18:20:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ echo "Please enter mode. Expected Values are server or client"
 read -r MODE_INPUT
 export MODE=$MODE_INPUT
 
-if [[ $MODE = client ]] ; then
+if [ -z $MODE ] then 
+echo echo " Mode not declared. exiting!"
+elif [[ $MODE = client ]] ; then
 echo "place holder for client"
 #docker run -d -e $MODE --env-file env.list --name sdwan deviantlinux/sdwan:latest
 # or
